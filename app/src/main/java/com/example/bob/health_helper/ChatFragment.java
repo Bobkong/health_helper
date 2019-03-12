3311b40988152ca9328265ff771a77af435e80b8
Bobkong <lingshuang.kong@cootek.cn>
2019-03-12 21:37:04 +0800

    as a test

--- app/src/main/java/com/example/bob/health_helper/ChatFragment.java
+++ app/src/main/java/com/example/bob/health_helper/ChatFragment.java
@@ -17,6 +17,7 @@ public class ChatFragment extends Fragment {
 	@Override
 	public void onCreate(@Nullable Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		//test
 	}
 	@Nullable
 	@Override
